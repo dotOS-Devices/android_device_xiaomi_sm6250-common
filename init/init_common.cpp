2865fa7ab3b2c099ac74806e5299cf4e37d1bba9
Jyotiraditya Panda <jyotiraditya@aospa.co>
2021-06-30 04:13:20 +0000

    sm6250-common: Override few audio props via init

--- init/init_common.cpp
+++ init/init_common.cpp
@@ -73,6 +73,11 @@ void load_dalvik_properties() {
     property_override("dalvik.vm.heapmaxfree", heapmaxfree);
 }
 
+void load_audio_properties() {
+    property_override("persist.vendor.audio.voicecall.speaker.stereo", "false");
+    property_override("vendor.audio.offload.track.enable", "false");
+}
+
 void load_common_properties() {
     load_dalvik_properties();
 }
