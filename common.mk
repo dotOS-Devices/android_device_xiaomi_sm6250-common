1ae9ab3923e65a44c9b149fdcd7f2193ff179fea
dereference23 <dereference23@outlook.com>
2021-10-02 12:54:00 +0000

    sm6250-common: Update camera provider service to 2.5.

--- common.mk
+++ common.mk
@@ -97,8 +97,7 @@ PRODUCT_PACKAGES += \
     GCamGOPrebuilt
 
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64
+    android.hardware.camera.provider@2.5-service_64
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
